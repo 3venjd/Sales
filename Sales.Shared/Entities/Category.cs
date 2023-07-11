e14241dbb14ce05d9c2e6bd6e46c3feb02aef164
3venjd <lui.eve.jim.dur@gmail.com>
2023-07-11 11:26:25 -0500

    Add States --> Cities and states quantity

--- Sales.Shared/Entities/Category.cs
+++ Sales.Shared/Entities/Category.cs
@@ -1,18 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Sales.Shared.Entities
 {
-    public class Country
+    public class Category
     {
         public int Id { get; set; }
 
-        [Display(Name = "Country")]
+        [Display(Name = "Category")]
         [Required(ErrorMessage = "The field {0} is required")]
         [MaxLength(100, ErrorMessage = "The field {0} can't have more than {1} characters")]
         public string Name { get; set; } = null!;
-
-        public ICollection<State>? States { get; set; }
-
-        public int StatesNumber => States == null ? 0 : States.Count;
     }
 }
