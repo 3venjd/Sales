a29fcf1890b07ba4964dc9524b1b059940f1deda
luisj@sciww.com <luisj@sciww.com>
2023-07-28 18:37:51 -0500

    loading DB with SQL

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllers()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=SQLConnection"));
 builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped<IApiService, ApiService>(); 
 
